1c61bd69dc630f8b95567d971be39530ebcb0361
tenmakamatani <tenma_18n1102540@nnn.ed.jp>
2019-11-04 13:00:21 +0900

    Add dotenv

--- src/models/sequelize-loader.js
+++ src/models/sequelize-loader.js
@@ -2,7 +2,19 @@
 
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('mysql://root:root@localhost/mokuview', {
+// Enable environment values
+import dotenv from 'dotenv';
+dotenv.config();
+
+const DB_DRIVER = process.env.DB_DRIVER;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+
+// Connect DB
+const connectInfo = `${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`;
+const sequelize = new Sequelize(connectInfo, {
   operatorsAliases: false,
 });
 
